87da019f272da5879bd62710244cf769ff21b87a
Tyler Main <tyler.l.main@gmail.com>
2022-08-23 01:50:56 -0500

    styling map and error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './App.css'
 
 class App extends React.Component {
 	constructor(props) {
@@ -10,6 +13,7 @@ class App extends React.Component {
 				cityData: [],
 				cityLon: '',
 				cityLat: '',
+				displayMap: ``,
 				error: false,
 				errorMessages: ''
 			}
@@ -33,35 +37,60 @@ class App extends React.Component {
 
 				let cityData = await axios.get(url);
 
+				let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityLat},${this.state.cityLon}&zoom=14&size=400x400`
+
 				console.log(cityData.data[0]);
 				console.log(cityData);
-				this.setState({cityData: cityData.data[0]});
-				this.setState({cityLon: cityData.data[0].lon});
-				this.setState({cityLat: cityData.data[0].lat});
-	}
+				
+				this.setState({
+					displayName: cityData.data[0].display_name,
+					cityLat: cityData.data[0].lat,
+					cityLon: cityData.data[0].lon,
+					displayMap: cityMap
+				})
+	} 
+	
+	catch(error){
+		this.setState({
+			error: true,
+			errorMessage: `${error.message}`
+		})
+	};
+
+handleClose = () => {
+	this.setState({error: false});
+};
 
 render() {
 	return (
-		<>
-		<form onSubmit={this.getCityData}>
-			<label> Pick a city!
-				<input type="text" onInput={this.handleInput} />
-			</label>
-			<button type='submit'>Explore!</button>
-		</form>
+		<div>
+		<header>
+		<Form onSubmit={this.getCityData}>
+			<Form.Label className='label'> Pick a city!
+				<input className='city' type="text" onInput={this.handleInput} />
+			</Form.Label>
+			<Button className='explore' type='submit'>Explore!</Button>
+		</Form>
+		</header>
 
+		<main>
 		<Card style={{ width: '18rem' }}>
+			<Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityLat},${this.state.cityLon}&zoom=14&size=400x400`} alt = 'Picture of map' />
 			<Card.Body>
 				<Card.Title>{this.state.city}</Card.Title>
-				<Card.Text>
+				<div className='lonlat'>
+				<Card.Text classname='text'>
 					Longitude: {this.state.cityLon}
-					<></>
+				
 					Latitude: {this.state.cityLat}
 				</Card.Text>
+				</div>
 			</Card.Body>
 		</Card>
-		</>
+		</main>
+		</div>
 	);
 	}
 }
+
 export default App;
